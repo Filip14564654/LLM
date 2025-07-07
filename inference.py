f7f340242841c9b843850e3db8c6760b2af22b2f
Filip Jína <118005701+Filip14564654@users.noreply.github.com>
2025-07-07 22:00:53 +0200

    commit

--- inference.py
+++ inference.py
@@ -4,7 +4,6 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import json
 import yaml
-from tokenizer.test_bpe_tokenizer import BPETokenizer
 from training.test_train import TransformerLanguageModel  # přizpůsob cestu podle umístění modelu
 from tokenizer.char_tokenizer import CharTokenizer  # nový tokenizer
 
